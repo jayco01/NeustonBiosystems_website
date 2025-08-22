980859de8cba80ccd99c2ee199de16e4f29ea3cf
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-08-22 17:56:15 -0600

    Feat: column1

--- src/components/layout/Footer/Column2.jsx
+++ src/components/layout/Footer/Column2.jsx
@@ -3,7 +3,14 @@ import {Link} from 'react-router-dom';
 export default function Column2() {
     return (
         <div>
-
+            <h6> Quick Links</h6>
+            <ul>
+                <li>Home Page</li>
+                <li>For Cheese Producers</li>
+                <li>For Investors</li>
+                <li>Sustainability</li>
+                <li>About Us</li>
+            </ul>
         </div>
     )
 }
---
+++ src/components/layout/Footer/Column3.jsx
@@ -0,0 +1,13 @@
+import {Link} from 'react-router-dom';
+
+export default function Column3() {
+    return (
+        <div>
+            <h6> Stay Connected</h6>
+            <ul>
+                <li>Contact Us</li>
+                <li>LinkedIn</li>
+            </ul>
+        </div>
+    )
+}
