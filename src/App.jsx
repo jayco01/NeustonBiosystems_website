b21b7310932cfbe1b8cf6e795f94f68d16bc4107
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-08-22 17:49:41 -0600

    Feat: column1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import {Routes,  Route} from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/layout/Navbar.jsx";
 import HomePage from "./pages/HomePage";
 import AboutUsPage from "./pages/AboutUsPage";
 import CareersPage from "./pages/CareersPage";
---
+++ src/components/layout/Footer/Column1.jsx
@@ -0,0 +1,16 @@
+import neustonLogo from ".../assets/NeustonBiosystemsLOGO.png"
+import {Link} from "react-router-dom";
+export default function Column1() {
+    return (
+        <div>
+            <div>
+                <Link to="/">
+                    <img src={neustonLogo} alt="Neuston Biosystems Logo" />
+                </Link>
+            </div>
+            <div>
+                <p>Subscribe to our newsletter for the latest updates on features and innovations.</p>
+            </div>
+        </div>
+    )
+}
