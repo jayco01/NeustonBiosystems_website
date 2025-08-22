6b19a0522a4812c261c8dd95843ab05c77f84778
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-08-22 17:17:50 -0600

    Feat: Setup Tailwind

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,19 +2,37 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="container">
-                <Link className="navbar-brand" to="/">Neuston Biosystems</Link>
-                <div className="space-x-6">
-                    <Link to="/" className="text-gray-600 hover:text-green-800">Home</Link>
-                    <Link to="/for-cheese-producers" className="text-gray-600 hover:text-green-800">For Cheese Producers</Link>
-                    <Link to="/for-investors" className="text-gray-600 hover:text-green-700">For Investors</Link>
-                    <Link to="/sustainability" className="text-gray-600 hover:text-green-700">Sustainability</Link>
-                    <Link to="/about" className="text-gray-600 hover:text-green-700">About Us</Link>
-                    <Link to="/careers" className="text-gray-600 hover:text-green-700">Careers</Link>
-                    <Link to="/contact" className="text-gray-600 hover:text-green-700">Contact</Link>
+        <header className="bg-white shadow-sm sticky top-0 z-50">
+            <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center h-20">
+                <div>
+                    <Link to="/" className="text-2xl font-bold text-gray-800 hover:text-gray-600">
+                        Neuston Biosystems
+                    </Link>
                 </div>
-            </div>
-        </nav>
+                <div className="md:flex items-center space-x-8">
+                    <Link to="/" className="text-base font-medium text-gray-500 hover:text-gray-900">
+                        Home
+                    </Link>
+                    <Link to="/for-investors" className="text-base font-medium text-gray-500 hover:text-gray-900">
+                        For Investors
+                    </Link>
+                    <Link to="/for-cheese-producers" className="text-base font-medium text-gray-500 hover:text-gray-900">
+                        For Cheesemakers
+                    </Link>
+                    <Link to="/sustainability" className="text-base font-medium text-gray-500 hover:text-gray-900">
+                        Sustainability
+                    </Link>
+                    <Link to="/about" className="text-base font-medium text-gray-500 hover:text-gray-900">
+                        About Us
+                    </Link>
+                    <Link to="/careers" className="text-base font-medium text-gray-500 hover:text-gray-900">
+                        Careers
+                    </Link>
+                    <Link to="/contact" className="text-base font-medium text-gray-500 hover:text-gray-900">
+                        Contact Us
+                    </Link>
+                </div>
+            </nav>
+        </header>
     );
 }
