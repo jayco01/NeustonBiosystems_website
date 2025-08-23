2817f8b09dadd3381fa8303e144c4963f4936b04
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-08-22 19:15:00 -0600

    Feat: HomeCta.jsx skeleton is done

---
+++ src/components/home/HomeCta.jsx
@@ -0,0 +1,19 @@
+import {Link} from "react-router-dom";
+
+export default function HomeCta() {
+    return (
+        <div>
+            <div>
+                <h2>Explore Your Opportunities Today</h2>
+            </div>
+            <div>
+                <p>Join us in transforming the food industry through innovative waste management solutions.
+                    Whether you're an investor or a cheese producers, we have opportunities tailored for you.</p>
+                <div>
+                    <Link to={'/for-investors'}>For Investors</Link>
+                    <Link to={'/for-cheese-producers'}>For Cheese Producers</Link>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import HomeHero from "../components/home/HomeHero.jsx";
 import HomeBody from "../components/home/HomeBody.jsx";
+import HomeCta from "../components/home/HomeCta.jsx";
 export default function HomePage() {
     return (
         <>
             <HomeHero/>
             <HomeBody/>
+            <HomeCta/>
         </>
     );
 }
