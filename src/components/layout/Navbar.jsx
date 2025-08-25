e7da08025d5620171616643166d566d8d6896490
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-08-24 20:40:19 -0600

    Feat: finished nav menu

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -3,14 +3,14 @@ import neustonLogo from "../../assets/NeustonBiosystemsLOGO.png"
 
 export default function Navbar() {
     return (
-        <header className="bg-neuston-Amber-Lightest sticky top-0 z-50">
-            <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center h-20">
+        <header className="bg-neuston-Amber-Lighter sticky top-0 z-50">
+            <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center h-[72px]">
                 <div>
                     <Link to="/" className="">
                         <img src={neustonLogo} className="max-w-21 max-h-10" alt="Neuston Biosystems logo" />
                     </Link>
                 </div>
-                <div className="md:flex items-center space-x-8 h-full w-full">
+                <div className="md:flex items-center space-x-5 h-full">
                     <Link to="/" className="nav-btn">
                             Home
                     </Link>
