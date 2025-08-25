72e2da75ef8c65a2a6b448df88e5b0bd92894955
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-08-24 20:23:58 -0600

    Feat: Added layer base

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -3,33 +3,33 @@ import neustonLogo from "../../assets/NeustonBiosystemsLOGO.png"
 
 export default function Navbar() {
     return (
-        <header className="bg-white shadow-sm sticky top-0 z-50">
+        <header className="bg-neuston-Amber-Lightest sticky top-0 z-50">
             <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center h-20">
                 <div>
                     <Link to="/" className="">
-                        <img src={neustonLogo} alt="Neuston Biosystems logo" />
+                        <img src={neustonLogo} className="max-w-21 max-h-10" alt="Neuston Biosystems logo" />
                     </Link>
                 </div>
-                <div className="md:flex items-center space-x-8">
-                    <Link to="/" className="text-base font-medium text-gray-500 hover:text-gray-900">
-                        Home
+                <div className="md:flex items-center space-x-8 h-full w-full">
+                    <Link to="/" className="nav-btn">
+                            Home
                     </Link>
-                    <Link to="/for-cheese-producers" className="text-base font-medium text-gray-500 hover:text-gray-900">
+                    <Link to="/for-cheese-producers" className="nav-btn">
                         For Cheese Producers
                     </Link>
-                    <Link to="/for-investors" className="text-base font-medium text-gray-500 hover:text-gray-900">
+                    <Link to="/for-investors" className="nav-btn">
                         For Investors
                     </Link>
-                    <Link to="/sustainability" className="text-base font-medium text-gray-500 hover:text-gray-900">
+                    <Link to="/sustainability" className="nav-btn">
                         Sustainability
                     </Link>
-                    <Link to="/about" className="text-base font-medium text-gray-500 hover:text-gray-900">
+                    <Link to="/about" className="nav-btn">
                         About Us
                     </Link>
-                    <Link to="/careers" className="text-base font-medium text-gray-500 hover:text-gray-900">
+                    <Link to="/careers" className="nav-btn">
                         Careers
                     </Link>
-                    <Link to="/contact" className="text-base font-medium text-gray-500 hover:text-gray-900">
+                    <Link to="/contact" className="nav-btn">
                         Contact Us
                     </Link>
                 </div>
