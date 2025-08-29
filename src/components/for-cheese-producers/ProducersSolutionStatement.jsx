8e8da44379741e845f5d67ea478f081c3b1dd56e
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-08-29 15:32:59 -0600

    Style: ProducersSolutionStatement.jsx MVP

--- src/components/for-cheese-producers/ProducersSolutionStatement.jsx
+++ src/components/for-cheese-producers/ProducersSolutionStatement.jsx
@@ -1,15 +1,15 @@
-import productDiagram from "../../assets/CircularEconomy_diagram.png";
+import productDiagram from "../../assets/neuston-01.jpg";
 
 export default function ProducersSolutionStatement() {
     return (
-        <section>
-            <img src={productDiagram} />
-            <div>
+        <section className="fluid-layout-padding flex flex-col md:flex-row justify-center items-center md:gap-20 gap-12 text-neuston-Amber-Darkest">
+            <img src={productDiagram} className="flex-1" />
+            <div className="flex-1 flex flex-col gap-6">
                 <h2>A Simpler, Scalable On-Site System.</h2>
-                <div>
+                <div className="flex-1 flex flex-col gap-3">
                     <p>A small, on-site water treatment plant that uses natural algae to clean your water and create a new product for you to use or sell.</p>
                     <p>Our system is designed to be affordable and easy to operate, fitting seamlessly into your existing production space.
-                        <span>By using a natural, self-regulating process, we eliminate the complexity and high costs of traditional systems, </span>
+                        <span className="text-content-highlight">By using a natural, self-regulating process, we eliminate the complexity and high costs of traditional systems, </span>
                         allowing you to focus on what you do best: making great cheese.</p>
                 </div>
             </div>
