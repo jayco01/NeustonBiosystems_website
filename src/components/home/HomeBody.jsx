aaa3fc4a3e0ecf8741830906a288405d4b4a1072
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-08-29 11:35:55 -0600

    Styles: HomeBody.jsx Mvp

--- src/components/home/HomeBody.jsx
+++ src/components/home/HomeBody.jsx
@@ -4,25 +4,25 @@ import joinIcon from "../../assets/join_icon.png"
 
 export default function HomeBody() {
     return (
-        <section className="fluid-layout-padding">
-            <div>
-                <h2>Addressing Waste Management Challenges for Cheese Producers</h2>
-                <p>Mid-scale Cheese producers face significant challenges in managing their waste effectively.
+        <section className="flex flex-col items-center fluid-layout-padding gap-20 bg-neuston-Amber-Lightest text-center">
+            <div className="max-w-4/5 mx-auto">
+                <h2 className="">Addressing Waste Management Challenges for Cheese Producers</h2>
+                <p className="max-w-3/4 mx-auto text-neuston-Amber-Darkest">Mid-scale Cheese producers face significant challenges in managing their waste effectively.
                     Traditional solutions are often too costly, leaving them with limited options.</p>
             </div>
-            <div>
-                <div>
-                    <img src={recycleIcon} alt="Recycle icon" />
-                    <h4>Revolutionizing Wastewater Treatment for Sustainability</h4>
+            <div className="flex md:flex-row flex-col gap-12 text-neuston-Amber-Darkest">
+                <div className="flex flex-col items-center gap-6">
+                    <img src={recycleIcon} alt="Recycle icon" className="icon-base-style"/>
+                    <h4 >Revolutionizing Wastewater Treatment for Sustainability</h4>
                     <p>Our innovative technology purifies wastewater while generating valuable byproducts.</p>
                 </div>
-                <div>
-                    <img src={profitIcon} alt="Profit icon" />
+                <div className="flex flex-col items-center gap-6">
+                    <img src={profitIcon} alt="Profit icon" className="icon-base-style"/>
                     <h4>Creating Opportunities Through Sustainable Practices</h4>
                     <p>We turn waste management costs into profitable opportunities.</p>
                 </div>
-                <div>
-                    <img src={joinIcon} alt="Join icon" />
+                <div className="flex flex-col items-center gap-6">
+                    <img src={joinIcon} alt="Join icon" className="icon-base-style"/>
                     <h4>Join Us in Making Sustainability Profitable</h4>
                     <p>Partner with us to embrace a sustainable future.</p>
                 </div>
