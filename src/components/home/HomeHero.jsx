2fc5d34e82fbf36c6e2688aaae83fbd1b11fac1b
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-08-29 10:43:32 -0600

    Style: Home Heading mvp

--- src/components/home/HomeHero.jsx
+++ src/components/home/HomeHero.jsx
@@ -10,21 +10,21 @@ export default function HomeHero() {
                 style={{backgroundImage: `url(${heroImage})`}}
             />
 
-            <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50" />
+            <div className="absolute top-0 left-0 w-full h-full bg-black opacity-70" />
 
             <div className="relative z-10 fluid-layout-padding">
-                <div className="flex flex-col items-start gap-24">
+                <div className="flex flex-col items-start gap-6">
                     <div className="flex flex-col items-start">
                         <h1>From Waste Stream</h1>
                         <h1>To <span className="text-neuston-Amber">Revenue Stream</span></h1>
                     </div>
                     <div className="">
-                        <p className="text-neuston-Amber-Lightest">At Neuston Biosystems, we transform wastewater challenges into profitable solutions for food producers.
+                        <p className="text-neuston-Amber-Lightest max-w-3/4">At Neuston Biosystems, we transform wastewater challenges into profitable solutions for cheese producers.
                             Join us in revolutionizing sustainability in the cheese industry.</p>
                     </div>
-                    <div className="flex flex-row gap-16">
-                        <Link to={'/for-investors'}>For Investors</Link>
-                        <Link to={'/for-cheese-producers'}>For Cheese Producers</Link>
+                    <div className="flex md:flex-row flex-col gap-4">
+                        <Link to={'/for-investors'} className="bg-neuston-Amber-Lightest text-neuston-Amber-Darkest btn-base-style">For Investors</Link>
+                        <Link to={'/for-cheese-producers'} className="bg-transparent text-neuston-Amber-Lightest btn-base-style border-solid border-neuston-Amber-Lightest border-2">For Cheese Producers</Link>
                     </div>
                 </div>
             </div>
