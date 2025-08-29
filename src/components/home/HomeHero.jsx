230a85cd4b78ccc9a1e2aad36107f8970f3d061c
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-08-28 18:00:55 -0600

    Fix: Order of HomeHero

--- src/components/home/HomeHero.jsx
+++ src/components/home/HomeHero.jsx
@@ -3,27 +3,35 @@ import heroImage from "../../assets/water-treatment-plant_home-page.jpg"
 
 export default function HomeHero() {
     return (
-        <section className="relative h-screen">
-            <img
-                src={heroImage}
-                alt="Water Treatment Plant"
-                className="absolute inset-0 w-full h-full object-cover"
+        // <section className="h-screen z-0 ">
+        <section className="relative flex items-center justify-center h-160 text-center">
+            {/*<img*/}
+            {/*    src={heroImage}*/}
+            {/*    alt="Water Treatment Plant"*/}
+            {/*    className="inset-0 w-full h-full object-cover"*/}
+            {/*/>*/}
+
+            <div
+                className="absolute top-0 left-0 w-full h-full bg-cover bg-center"
+                style={{backgroundImage: `url(${heroImage})`}}
             />
 
-            <div className="absolute inset-0 bg-black/50"></div>
+            <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50" />
 
-            <div className="relative h-full flex flex-col">
-                <div>
-                    <h1>From Waste Stream</h1>
-                    <h1>To Revenue Stream</h1>
-                </div>
-                <div>
-                    <p>At Neuston Biosystems, we transform wastewater challenges into profitable solutions for food producers.
-                        Join us in revolutionizing sustainability in the cheese industry.</p>
-                </div>
-                <div>
-                    <Link to={'/for-investors'}>For Investors</Link>
-                    <Link to={'/for-cheese-producers'}>For Cheese Producers</Link>
+            <div className="relative z-10">
+                <div className="home-heading-text-spacing">
+                    <div className="home-heading-text-spacing">
+                        <h1 className="">From Waste Stream</h1>
+                        <h1>To Revenue Stream</h1>
+                    </div>
+                    <div className="home-heading-text-spacing">
+                        <p>At Neuston Biosystems, we transform wastewater challenges into profitable solutions for food producers.
+                            Join us in revolutionizing sustainability in the cheese industry.</p>
+                    </div>
+                    <div className="home-heading-text-spacing">
+                        <Link to={'/for-investors'}>For Investors</Link>
+                        <Link to={'/for-cheese-producers'}>For Cheese Producers</Link>
+                    </div>
                 </div>
             </div>
         </section>
