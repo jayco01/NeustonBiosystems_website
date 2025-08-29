bba2860767e5d53d8ccbf308b691a89a3538e6a4
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-08-29 12:54:01 -0600

    Style: Update Styling for CommonHero.jsx

--- src/components/common/CommonHero.jsx
+++ src/components/common/CommonHero.jsx
@@ -1,15 +1,14 @@
 export default function CommonHero({heroImage, H1Text, subHeading}) {
     return (
-        <section className="relative h-screen">
-            <img
-                src={heroImage}
-                alt="Water Treatment Plant"
-                className="absolute inset-0 w-full h-full object-cover"
+        <section className="relative flex items-center h-80">
+            <div
+                className="absolute top-0 left-0 w-full h-full bg-cover bg-center"
+                style={{backgroundImage: `url(${heroImage})`}}
             />
 
-            <div className="absolute inset-0 bg-black/50"></div>
+            <div className="absolute top-0 left-0 w-full h-full bg-black opacity-70"></div>
 
-            <div className="relative h-full flex flex-col">
+            <div className="relative z-10 fluid-layout-padding">
                 <div>
                     <H1Text/>
                 </div>
