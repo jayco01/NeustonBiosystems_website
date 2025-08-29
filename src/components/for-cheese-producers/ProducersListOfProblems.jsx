fd4095caee8e0335e6dc295ac0657b82ef651675
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-08-29 15:05:45 -0600

    Style: ProducersListOfProblems.jsx MVP

--- src/components/for-cheese-producers/ProducersListOfProblems.jsx
+++ src/components/for-cheese-producers/ProducersListOfProblems.jsx
@@ -4,21 +4,22 @@ import chemicalPlantIcon from "../../assets/ChemicalPlant_Icon.png";
 
 
 export default function ProducersListOfProblems() {
+    const cardStyle = "bg-neuston-Amber-Lightest flex flex-col justify-start items-center md:gap-6 gap-3 px-3 py-3 rounded-lg text-center text-neuston-Amber-Darkest flex-1"
     return (
-        <section>
-            <div>
+        <section className="fluid-layout-padding bg-neuston-Amber flex flex-col md:flex-row gap-6">
+            <div className={cardStyle}>
                 <img src={truckIcon} alt="truck icon" />
-                <h3>Hauling</h3>
+                <h4>Hauling</h4>
                 <p>Annual off-site hauling costs can range from $50,000 to $500,000.</p>
             </div>
-            <div>
+            <div className={cardStyle}>
                 <img src={sewerIcon} alt="sewer icon" />
-                <h3>Sewer Surcharges</h3>
+                <h4>Sewer Surcharges</h4>
                 <p>Municipal sewer surcharges can cost up to $50 per 1,000 liters.</p>
             </div>
-            <div>
+            <div className={cardStyle}>
                 <img src={chemicalPlantIcon} alt="chemical plant icon" />
-                <h3>On-Site Systems</h3>
+                <h4>On-Site Systems</h4>
                 <p>100% of producers we surveyed have tried and failed to buy an affordable on-site system in the last 10 years.</p>
             </div>
         </section>
--- src/components/for-cheese-producers/ProducersProblemStatement.jsx
+++ src/components/for-cheese-producers/ProducersProblemStatement.jsx
@@ -3,8 +3,8 @@ import mixingCheese from "../../assets/CheeseWhey.jpg"
 export default function ProducersProblemStatement() {
     return (
       <section className="fluid-layout-padding bg-neuston-Amber-Lightest flex flex-col md:flex-row justify-center items-center md:gap-20 gap-12">
-          <div className="flex-1 flex flex-col gap-8">
-              <h2 className="text-9xl">A Costly Mid-Scale Dilemma</h2>
+          <div className="flex-1 flex flex-col md:gap-8 ">
+              <h2 className="xl:text-9xl">A Costly Mid-Scale Dilemma</h2>
               <p>As a mid-sized cheese producers, you're likely caught in a tough spot.
                   <span className="text-content-highlight">You produce too much whey for simple disposal, yet not enough to justify a large-scale treatment system.</span>
               </p>
