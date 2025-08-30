d1b2fae2e22efd1120bac69bd9a1cb8f6b290854
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-08-29 18:24:01 -0600

    Style: InvestorsFundingNumbers.jsx MVP

--- src/components/for-investors/InvestorsFundingNumbers.jsx
+++ src/components/for-investors/InvestorsFundingNumbers.jsx
@@ -1,10 +1,12 @@
 export default function InvestorsFundingNumbers() {
     return (
-        <section>
-            <h2>Join Us at the Pre-Seed Stage.</h2>
-            <div>
+        <section className="fluid-layout-padding bg-neuston-Amber-Lighter text-neuston-Amber-Darkest flex flex-col md:flex-row md:gap-20 gap-6">
+            <div className="flex-1 inline-flex md:justify-end">
+                <h2 className="max-w-130">Join Us at the Pre-Seed Stage</h2>
+            </div>
+            <div className="flex-1">
                 <p>Invest in a sustainable future with Neuston Biosystems. Together, we can transform waste into valuable resources.</p>
-                <div>
+                <div className="flex flex-col md:flex-row md:gap-6 gap-3 mt-3">
                     <div>
                         <h4>The Ask</h4>
                         <p>We are seeking $300,000 to advance our prototype to a pilot-ready system.</p>
