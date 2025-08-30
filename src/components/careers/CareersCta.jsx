7f4b67228477bec9e520340965fbf573b7569b3c
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-08-29 20:18:25 -0600

    Style: CareersCta.jsx MVP

--- src/components/careers/CareersCta.jsx
+++ src/components/careers/CareersCta.jsx
@@ -1,10 +1,10 @@
 export default function CareersCta() {
     return (
-        <section>
+        <section className="fluid-layout-padding bg-neuston-Amber-Lighter text-neuston-Amber-Darkest flex flex-col gap-4 md:gap-8 items-center">
             <h2>Apply for Your Future</h2>
             <p>
                 Send your resume and cover letter to
-                <span>careers@neustonbiosystems.com</span>
+                <span> careers@neustonbiosystems.com </span>
                 to join our team.
             </p>
         </section>
