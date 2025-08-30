5717cb8985b886173fdc87b95a01600d066773c4
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-08-29 19:45:32 -0600

    Style: SustainabilityCircularEconomy.jsx MVP

--- src/components/sustainability/SustainabilityCircularEconomy.jsx
+++ src/components/sustainability/SustainabilityCircularEconomy.jsx
@@ -2,12 +2,12 @@ import circularEconomyDiagram from "../../assets/CircularEconomy_diagram.png"
 
 export default function SustainabilityCircularEconomy() {
     return (
-      <section>
-          <img src={circularEconomyDiagram} alt="Circular Economy Diagram" />
-          <div>
+      <section className="fluid-layout-padding text-neuston-Amber-Darkest bg-neuston-Amber-Lightest flex flex-col md:flex-row items-center md:gap-20 gap-10">
+          <img src={circularEconomyDiagram} alt="Circular Economy Diagram" className=" md:max-w-150 max-w-11/12"/>
+          <div className="flex flex-col gap-4 md:gap-8">
               <h2>Enabling a True Circular Economy</h2>
               <p>Our technology does more than just treat waste. It closes the loop.
-                  <span>We transform pollutants into valuable resources that can be reintroduced into the agricultural system,</span>
+                  <span className="text-content-highlight">We transform pollutants into valuable resources that can be reintroduced into the agricultural system,</span>
                   reducing environmental impact and creating new value for producers.</p>
           </div>
       </section>
--- src/components/sustainability/SustainabilitySolutions.jsx
+++ src/components/sustainability/SustainabilitySolutions.jsx
@@ -5,25 +5,31 @@ export default function SustainabilitySolutions() {
         <section className="fluid-layout-padding text-neuston-Amber-Darkest bg-neuston-Amber-Lighter">
             <ul className="list-none">
                 <li className="flex items-start p-3 m-3 bg-neuston-Amber-Light rounded-xl">
+                    <div className="flex flex-row items-center">
                     <img src={checkIcon} alt="Check Icon"/>
                     <div>
                         <h4>Protecting Waterways</h4>
                         <p>Our process reclaims phosphorus—a limited resource and harmful pollutant—without the need for costly chemicals.</p>
                     </div>
+                        </div>
                 </li>
                 <li className="flex items-start p-3 m-3 bg-neuston-Amber-Light rounded-xl">
+                    <div className="flex flex-row items-center">
                     <img src={checkIcon} alt="Check Icon"/>
                     <div>
                         <h4>Reducing Energy Use</h4>
                         <p>We treat high-load waste streams efficiently, avoiding the high energy costs associated with traditional aeration systems.</p>
                     </div>
+                        </div>
                 </li>
                 <li className="flex items-start p-3 m-3 bg-neuston-Amber-Light rounded-xl">
+                    <div className="flex flex-row items-center">
                     <img src={checkIcon} alt="Check Icon"/>
                     <div>
                         <h4>Creating Sustainable Products</h4>
                         <p>The microalgae from our system is a nutrient-rich, sustainable alternative for biofertilizers and animal feed.</p>
                     </div>
+                        </div>
                 </li>
             </ul>
         </section>
