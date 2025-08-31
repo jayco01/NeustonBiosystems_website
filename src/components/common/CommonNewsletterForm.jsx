122b1692d22ae7c6aa4a2bd609ae557dc64fac8e
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-08-31 14:30:54 -0600

    Styles: Updated Newsletter placeholder

--- src/components/common/CommonNewsletterForm.jsx
+++ src/components/common/CommonNewsletterForm.jsx
@@ -1,6 +1,7 @@
 export default function CommonNewsletterForm() {
-    // URL will be replaced ones Scott Finish signing up for mailerlite
-    const MAILERLITE_ACTION_URL = "https://assets.mailerlite.com/jsonp/your_unique_id/forms/your_form_id/subscribe";
+    // URL will be replaced ones Scott Finish signing up for BREVO
+    const BREVO_ACTION_URL = "Awaiting for Brevo Account";
+    //https://assets.brevo.com/jsonp/unique_id/forms/form_id/subscribe
 
     return (
         <section className="fluid-layout-padding bg-neuston-Amber-Lighter text-neuston-Amber-Darkest flex flex-col items-center gap-3">
@@ -10,14 +11,14 @@ export default function CommonNewsletterForm() {
             </p>
             <div>
                 <form
-                    action={MAILERLITE_ACTION_URL}
+                    action={BREVO_ACTION_URL}
                     method="post"
                     target="_blank" // this will eventually open a thank you page
                     className="mt-4 flex"
                 >
                     <input
                         type="email"
-                        name="fields[email]" // Email Input name will be replaced ones Scott Finish signing up for mailerlite
+                        name="fields[email]" // Email Input name will be replaced ones Scott Finish signing up for BREVO
                         placeholder="Enter your email"
                         required
                         className="w-full px-4 py-2 border text-neuston-Amber-Dark rounded-l-md focus:ring-green-500 focus:border-green-500 font-bold"
