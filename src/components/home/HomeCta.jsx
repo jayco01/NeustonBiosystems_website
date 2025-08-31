b6891a51ec3e5d4bd299eaddd1e72c33f92ce178
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-08-31 14:28:18 -0600

    Styles: Buttons to flex-row

--- src/components/home/HomeCta.jsx
+++ src/components/home/HomeCta.jsx
@@ -9,7 +9,7 @@ export default function HomeCta() {
             <div className="flex flex-1 flex-col gap-3">
                 <p className="">Join us in transforming the food industry through innovative waste management solutions.
                     Whether you're an investor or a cheese producers, we have opportunities tailored for you.</p>
-                <div className="flex flex-col lg:flex-row gap-1">
+                <div className="flex flex-row gap-1">
                     <Link to={'/for-investors'} className="btn-base-style bg-neuston-Amber">For Investors</Link>
                     <Link to={'/for-cheese-producers'} className="btn-base-style border-solid border-neuston-Amber-Darkest border-1">For Cheese Producers</Link>
                 </div>
