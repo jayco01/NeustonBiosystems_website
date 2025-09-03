d0778ff0b3bacc23a401273c2decfd46ef5f1e84
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-09-03 10:43:55 -0600

    Fix: Removed IP info

--- src/components/sustainability/SustainabilityCircularEconomy.jsx
+++ src/components/sustainability/SustainabilityCircularEconomy.jsx
@@ -1,9 +1,9 @@
-import circularEconomyDiagram from "../../assets/CircularEconomy_diagram.png"
+// import circularEconomyDiagram from "../../assets/CircularEconomy_diagram.png"
 
 export default function SustainabilityCircularEconomy() {
     return (
       <section className="fluid-layout-padding text-neuston-Amber-Darkest bg-neuston-Amber-Lightest flex flex-col md:flex-row items-center md:gap-20 gap-10">
-          <img src={circularEconomyDiagram} alt="Circular Economy Diagram" className=" md:max-w-150 max-w-11/12"/>
+          {/*<img src={circularEconomyDiagram} alt="Circular Economy Diagram" className=" md:max-w-150 max-w-11/12"/>*/}
           <div className="flex flex-col gap-4 md:gap-8">
               <h2>Enabling a True Circular Economy</h2>
               <p>Our technology does more than just treat waste. It closes the loop.
