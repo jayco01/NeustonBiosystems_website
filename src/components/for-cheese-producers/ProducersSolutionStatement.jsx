e9af1cf8d7a92b46b3dbe42dccbe0167ccbffd8a
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-09-03 10:36:39 -0600

    Fix: Removed IP info

--- src/components/for-cheese-producers/ProducersSolutionStatement.jsx
+++ src/components/for-cheese-producers/ProducersSolutionStatement.jsx
@@ -1,9 +1,9 @@
-import productDiagram from "../../assets/neuston-01.jpg";
+// import productDiagram from "../../assets/neuston-01.jpg";
 
 export default function ProducersSolutionStatement() {
     return (
         <section className="fluid-layout-padding flex flex-col md:flex-row justify-center items-center md:gap-20 gap-12 text-neuston-Amber-Darkest bg-neuston-Amber-Lightest">
-            <img src={productDiagram} className="flex-1" />
+            {/*<img src={productDiagram} className="flex-1" />*/}
             <div className="flex-1 flex flex-col gap-6">
                 <h2>A Simpler, Scalable On-Site System.</h2>
                 <div className="flex-1 flex flex-col gap-3">
