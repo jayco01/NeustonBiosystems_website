8b3bdd539c6de920f1a33395c2247123b5728e3c
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-09-07 21:04:33 -0600

    Feat: Connected Brevo service to newsletter

--- src/components/common/CommonNewsletterForm.jsx
+++ src/components/common/CommonNewsletterForm.jsx
@@ -1,40 +1,38 @@
-import { useNavigate } from 'react-router-dom';
-
 export default function CommonNewsletterForm() {
-    const navigate = useNavigate();
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        navigate('/thank-you');
-    };
+    const BREVO_ACTION_URL = "https://05d39b3e.sibforms.com/serve/MUIFAHB96fT6pdJ3kmdUEqGdaUCR5NsEfJNVgPE_iWZzX1kpYgjqiGGvMqot9pTHXgKkoVwavGwYlIqF382wpXGfw-lCRtcUIOEOtTdNo0vgcPcQC8qnSGlLa6BrtiaEq2GUSB-x7ZbbKrxL0nBiv1CpFqJDX2SgS7mWTPkEWQDy6uvPTF5dW9RcjaVEsqw9W1HkfbC14SWlUlz3";
 
     return (
         <section className="fluid-layout-padding bg-neuston-Amber-Lighter text-neuston-Amber-Darkest flex flex-col items-center gap-3">
             <h2>Stay Connected with Us</h2>
-            <p className="mt-2 text-neuston-Amber-Darkest">
+            <p className="mt-2 text-neuston-Amber-Darkest text-center">
                 Stay up to date with our progress and innovations in wastewater treatment technology.
             </p>
             <div>
                 <form
-                    onSubmit={handleSubmit}
+                    action={BREVO_ACTION_URL}
+                    method="post"
+                    target="_blank"
                     className="mt-4 flex"
                 >
                     <input
                         type="email"
-                        name="fields[email]"
+                        name="EMAIL"
                         placeholder="Enter your email"
                         required
                         className="w-full px-4 py-2 border text-neuston-Amber-Dark rounded-l-md focus:ring-green-500 focus:border-green-500 font-bold"
                     />
                     <button
                         type="submit"
-                        className="btn-base-style bg-neuston-Amber text-neuston-Amber-Darkest hover:bg-neuston-Amber-Lightest border-solid border-2 border-neuston-Amber rounded-r-md rounded-l-none"
+                        className="btn-base-style bg-neuston-Amber text-neuston-Amber-Darkest hover:bg-neuston-Amber-Lightest border-solid border-2 border-neuston-Amber text-nowrap"
                     >
                         Subscribe Now
                     </button>
                 </form>
-                <p className="text-sm mt-2">By clicking Subscribe Now, you agree to our Terms and Conditions.</p>
+                <p className="mt-2 text-xs text-gray-500">
+                    By clicking Subscribe Now, you agree to our Terms and Conditions.
+                </p>
             </div>
         </section>
     );
 }
+
